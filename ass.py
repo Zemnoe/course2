42d9edb1ccd9aea295f10495ba59df3993fc02ce
Zemnoe <nzemnoe@gmail.com>
2024-05-02 13:47:19 +0100

    assignment 4

---
+++ ass.py
@@ -0,0 +1,34 @@
+first_name = "Paschalina"
+middle_name = "Zemnoe"
+last_name = "Manomi"
+
+
+middle_name = "Zemnoe"
+print(type(middle_name))
+
+x = 5.5
+print(type(x)) 
+
+int = 12
+print(type(int))
+
+k = 3 + 5j
+print(type(k))
+
+z = [ " peace", "joy", "hope"]
+print (type(z))
+
+T = ("apple", "banana", "orange")
+print (type(T))
+
+R = range (6)
+print (type (R))
+
+dict = {"name": "jane", "age": 20}
+print(type(dict))
+
+S = {"joy", "Peace"}
+print(type(S))
+
+Y = True
+print(type(Y)) 
